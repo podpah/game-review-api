3334b5b9ee0a74db561f7b0f9c14230a11294305
haroldboi <preston.harry2003@gmail.com>
2022-11-02 15:57:47 +0000

    connecting to mongodb and creating first test route

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from dotenv import dotenv_values
+from flask import Flask,request
+from pymongo import MongoClient
+import certifi
 
 app = Flask(__name__)
+config = dotenv_values(".env")
+app.mongodb_client = MongoClient(config["ATLAS_URI"], tlsCAFile=certifi.where())
+db = app.mongodb_client.get_database("gratings")
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/test")
+def mainroute():
+    search = db.users.find_one({"id": 1})
+    return f"{search}"
