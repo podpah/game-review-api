2dcfda3ce99d8426df9c3aac3a3cbd5b81b053d9
haroldboi <preston.harry2003@gmail.com>
2022-11-02 16:06:50 +0000

    .env stuff

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from dotenv import dotenv_values
 from flask import Flask, request
 from pymongo import MongoClient
+import certifi
 
 app = Flask(__name__)
 config = dotenv_values(".env")
-app.mongodb_client = MongoClient(config["ATLAS_URI"])
+app.mongodb_client = MongoClient(config["ATLAS_URI"], tlsCAFile=certifi.where())
 db = app.mongodb_client.get_database("gratings")
 
 
