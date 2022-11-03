738fd6a25c59793b70fddb89bd23fe5082ffe7a8
podpah <stream_bars07@icloud.com>
2022-11-03 11:05:00 +0000

    Created delete route, optimised get route

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ app.mongodb_client = MongoClient(config["ATLAS_URI"], tlsCAFile=certifi.where())
 db = app.mongodb_client.get_database("gratings")
 
 
-@app.route("/entries/<int:idd>", methods=["GET", "PUT"])  # See all posts / Send a new post
+# If there is someone logged in then unauthorized is 403 but if not then it's 401
+
+@app.route("/entries/<int:idd>", methods=["GET", "PUT", "DELETE"])  # See all posts / Send a new post
 @app.route("/entries/", methods=["GET", "POST"])  # See all posts / Send a new post
 def mainroute(idd=0):
     if request.method == "POST":
@@ -22,14 +24,13 @@ def mainroute(idd=0):
         return jsonify({"Author: ": author, "Review:": review, "Game: ": game})
     elif request.method == "GET":
         if idd:
-            search = db.ratings_dev.find({"id": idd}, {"_id": 0})
-            search = list(search)
-            return f"{search} Test"
+            search = db.ratings_dev.find_one({"id": idd}, {"_id": 0})
+            return jsonify(search), 200
             # Would like to figure out how to separate it but that's not a very API thing to do
         else:
             search = db.ratings_dev.find({}, {"_id": 0})
             search = list(search)
-            return f"{search} Test"
+            return jsonify(search), 200
     elif request.method == "PUT":
         data = request.get_json()
         author = data["author"]
@@ -40,7 +41,7 @@ def mainroute(idd=0):
             return jsonify("ID not found"), 400
         else:
             if author != search["author"]:
-                return jsonify("Not authorised to edit this review"), 400
+                return jsonify("Not authorised to edit this review"), 403
             elif review == search["review"] and game == search["game"]:
                 return jsonify("The request body is the same as the current information"), 400
             else:
@@ -48,3 +49,12 @@ def mainroute(idd=0):
                 db.ratings_dev.update_one(search, newvals)
         search = db.ratings_dev.find_one({"id": idd}, {"_id": 0, "id": 0})
         return jsonify(search)
+    elif request.method == "DELETE":
+        data = request.get_json()
+        author = data["author"]
+        search = db.ratings_dev.find_one({"id": idd}, {"_id": 0})
+        if search["author"] != author:
+            return jsonify("Not authorised to delete this review"), 403
+        else:
+            teapot = db.ratings_dev.delete_one({"id": idd})
+            return jsonify({"Deleted review": search["review"]}), 418
