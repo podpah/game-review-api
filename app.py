0e991b584a5d16dd1622593534b0ff25767ab092
podpah <stream_bars07@icloud.com>
2022-11-30 12:22:14 +0000

    Final logic changes

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-
 import jwt
 import bcrypt
 import datetime
-
 from dotenv import dotenv_values
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 import certifi
 
 app = Flask(__name__)
-config = dotenv_values(".env")  # This links it to the .env file with your secret variables
+config = dotenv_values(".env")
+# This links it to the .env file with your secret variables
 # The certifi is here to prevent issues when connecting to mongo
-app.mongodb_client = MongoClient(config["ATLAS_URI"],tlsCAFile=certifi.where())  
+app.mongodb_client = MongoClient(config["ATLAS_URI"], tlsCAFile=certifi.where())
 db = app.mongodb_client.get_database("gratings")
 jwt_sec = config["JWT_SEC"]
 
@@ -23,21 +22,25 @@ def autho():
     try:
         bearer = request.headers.get("Authorization")
         bearer = bearer.split(" ")[-1]
-        decoded = jwt.decode(bearer, jwt_sec, algorithms="HS256") # Automatically checks whether or not the token "exp" is in the future
+        decoded = jwt.decode(
+            bearer, jwt_sec, algorithms="HS256"
+        )  # Automatically checks whether or not the token "exp" is in the future
         return decoded["author"]
     except jwt.ExpiredSignatureError:
         return "Expired"
     except:
         return "Not verified"
 
+
 def check_json():
     try:
         data = request.get_json()
         review = data["review"]
         game = data["game"]
-        return {review,game}
+        return {review, game}
     except:
-        return [None,None]
+        return [None, None]
+
 
 def check_admin(search2):
     try:
@@ -47,19 +50,38 @@ def check_admin(search2):
     finally:
         return truth
 
-@app.route("/entries/<int:idd>", methods=["GET", "PUT", "DELETE"])  # See all posts / Send a new post
+
+@app.route(
+    "/entries/<int:idd>", methods=["GET", "PUT", "DELETE"]
+)  # See all posts / Send a new post
 @app.route("/entries/", methods=["GET", "POST"])  # See all posts / Send a new post
 def mainroute(idd=0):
     authorise = autho()
     if authorise == "Not verified":
-        return jsonify({"message": "INVALID TOKEN , please get a valid token from the /login endpoint " }),400
+        return (
+            jsonify(
+                {
+                    "message": "INVALID TOKEN , please get a valid token from the /login endpoint "
+                }
+            ),
+            400,
+        )
     elif authorise == "Expired":
-        return jsonify({"message": "EXPIRED TOKEN , please get a new token from the /login endpoint " }),400
+        return (
+            jsonify(
+                {
+                    "message": "EXPIRED TOKEN , please get a new token from the /login endpoint "
+                }
+            ),
+            400,
+        )
     else:
-        review,game = check_json()
+        review, game = check_json()
         author = authorise
         if request.method == "POST":
-            db.ratings_dev.insert_one({"author": author, "review": review, "game": game})
+            db.ratings_dev.insert_one(
+                {"author": author, "review": review, "game": game}
+            )
             return jsonify({"Author: ": author, "Review:": review, "Game: ": game})
         elif request.method == "GET":
             if idd:
@@ -75,17 +97,22 @@ def mainroute(idd=0):
             if not search:
                 return jsonify("ID not found"), 400
             elif review == search["review"] and game == search["game"]:
-                return jsonify("The request body is the same as the current information"), 400
+                return (
+                    jsonify("The request body is the same as the current information"),
+                    400,
+                )
             elif search["author"] == author or check_admin(search2):
                 newvals = {"$set": {"game": game, "review": review}}
                 db.ratings_dev.update_one(search, newvals)
             elif author != search["author"]:
                 return jsonify("Not authorised to edit this review"), 403
             search = db.ratings_dev.find_one({"id": idd}, {"_id": 0, "id": 0})
-            return jsonify({"message":"Updated review successfully ", "review":search})
+            return jsonify(
+                {"message": "Updated review successfully ", "review": search}
+            )
         elif request.method == "DELETE":
             search = db.ratings_dev.find_one({"id": idd}, {"_id": 0})
-            search2 = db.users.find_one({"author":author}, {"_id": 0})
+            search2 = db.users.find_one({"author": author}, {"_id": 0})
             if search["author"] == author or check_admin(search2):
                 teapot = db.ratings_dev.delete_one({"id": idd})
                 return jsonify({"Deleted review": search["review"]}), 418
@@ -113,17 +140,24 @@ def register():
 def login():
     data = request.get_json()
     author = data["author"]
-    search = db.users.find_one({"author": author},{"_id":0})
+    search = db.users.find_one({"author": author}, {"_id": 0})
     if search is None:
-         return jsonify("Could not find account, check your spelling"), 401
+        return jsonify("Could not find account, check your spelling"), 401
     dbpass = search["passwd"]
     dbpass = dbpass.encode("utf-8")
     userPass = data["passwd"]
     userPass = userPass.encode("utf-8")
     passCheck = bcrypt.checkpw(userPass, dbpass)
     if passCheck:
-        print(datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(seconds=120))
-        token = jwt.encode({"author":search["author"], "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(seconds=120)},jwt_sec, algorithm="HS256")
-        return jsonify(f"Authorised. Welcome to Game Ratings {author}",token), 200
+        token = jwt.encode(
+            {
+                "author": search["author"],
+                "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+                + datetime.timedelta(hours=24),
+            },
+            jwt_sec,
+            algorithm="HS256",
+        )
+        return jsonify(f"Authorised. Welcome to Game Ratings {author}", token), 200
     else:
         return jsonify("Unauthorized"), 401
